5b41c61262e57fdd561f841a47232eb1d73786b1
Taylor Lovett <tlovett88@gmail.com>
2015-01-12 23:53:41 -0500

    Add form and submissions link to plugins table

--- classes/class-ccf-custom-contact-forms.php
+++ classes/class-ccf-custom-contact-forms.php
@@ -17,6 +17,27 @@ class CCF_Custom_Contact_Forms {
 	public function setup() {
 		add_action( 'plugins_loaded', array( $this, 'manually_load_api' ), 100 );
 		add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
+		add_filter( 'plugin_row_meta', array( $this, 'filter_plugin_row_meta' ), 10, 4 );
+	}
+
+	/**
+	 * Add forms and form submissions link to meta
+	 *
+	 * @param array $plugin_meta
+	 * @param string $plugin_file
+	 * @param string $plugin_data
+	 * @param string $status
+	 * @since 6.0
+	 * @return array
+	 */
+	public function filter_plugin_row_meta( $plugin_meta, $plugin_file, $plugin_data, $status ) {
+		$new_meta = array();
+
+		if ( 'custom-contact-forms/custom-contact-forms.php' === $plugin_file ) {
+			$new_meta['ccf_forms'] = sprintf( __( '<a href="%s">Forms and Submissions</a>', 'custom-contact-forms' ), esc_url( admin_url( 'edit.php?post_type=ccf_form' ) ) );
+		}
+
+		return array_merge( $new_meta, $plugin_meta );
 	}
 
 	/**
