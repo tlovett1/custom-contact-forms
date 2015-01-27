01514c1918311d7e5b7dd3c0f39ed3cb40534bf8
Taylor Lovett <tlovett88@gmail.com>
2015-01-27 17:55:21 +0100

    Checkbox tests

--- tests/js/test-form-errors.js
+++ tests/js/test-form-errors.js
@@ -201,6 +201,8 @@
 		});
 	});
 
+	// @Todo: test international phone number
+
 	QUnit.test( 'Test simple successful US address field', function( assert ) {
 		var done = assert.async();
 
@@ -297,6 +299,129 @@
 				}
 
 				equal( requiredErrorsFound, 3, 'Form has three address required errors' );
+				done();
+			});
+
+			// Submit form
+			$form.submit();
+		});
+	});
+
+	// Todo: Test international address
+
+	QUnit.test( 'Test simple unsuccessful incomplete US address field', function( assert ) {
+		var done = assert.async();
+
+		expect( 2 );
+
+		$( qunit ).load( 'forms/simple-us-address-1.html', function() {
+			wp.ccf.setupDOM();
+
+			var $form = $('.ccf-form' );
+
+			$form.on( 'ccfFormError', function() {
+				equal( arguments.length, 2, 'Form submitted with one error field' );
+
+				var errors = arguments[1].errors;
+
+				var requiredErrorsFound = 0;
+
+				for ( var errorKey in errors ) {
+					if ( errors[errorKey].required ) {
+						requiredErrorsFound++;
+					}
+				}
+
+				equal( requiredErrorsFound, 3, 'Form has three address required errors' );
+				done();
+			});
+
+			// Submit form
+			$form.submit();
+		});
+	});
+
+	QUnit.test( 'Test simple successful checkboxes field', function( assert ) {
+		var done = assert.async();
+
+		expect( 1 );
+
+		$( qunit ).load( 'forms/simple-checkboxes-1.html', function() {
+			wp.ccf.setupDOM();
+
+			var $form = $('.ccf-form' );
+
+			var $requiredFields = $form.find( '.field-required .field-input');
+			$requiredFields.each( function() {
+				// make sure we skip state field
+				$( this).attr( 'checked', 'checked' );
+			});
+
+			$form.on( 'ccfFormSuccess', function() {
+				ok( true, 'Form submitted without errors' );
+				done();
+			});
+
+			// Submit form
+			$form.submit();
+		});
+	});
+
+	QUnit.test( 'Test simple unsuccessful checkboxes field', function( assert ) {
+		var done = assert.async();
+
+		expect( 2 );
+
+		$( qunit ).load( 'forms/simple-checkboxes-1.html', function() {
+			wp.ccf.setupDOM();
+
+			var $form = $('.ccf-form' );
+
+			$form.on( 'ccfFormError', function() {
+				console.log( arguments );
+				equal( arguments.length, 2, 'Form submitted with one error field' );
+
+				var errors = arguments[1].errors;
+				var keys = _.keys( errors );
+
+				equal( _.keys( errors[keys[0]] )[0], 'required', 'Form has required error' );
+				done();
+			});
+
+			// Submit form
+			$form.submit();
+		});
+	});
+
+	QUnit.test( 'Test simple unsuccessful checkboxes field only dead option checked', function( assert ) {
+		var done = assert.async();
+
+		expect( 2 );
+
+		$( qunit ).load( 'forms/simple-checkboxes-1.html', function() {
+			wp.ccf.setupDOM();
+
+			var $form = $('.ccf-form' );
+
+			var $requiredFields = $form.find( '.field-input'),
+				i = 0;
+			$requiredFields.each( function() {
+				if ( i > 0 ) {
+					return false;
+				}
+
+				$( this ).attr( 'checked', 'checked' );
+
+				i++;
+			});
+
+			$form.on( 'ccfFormError', function() {
+				equal( arguments.length, 2, 'Form submitted with one error field' );
+
+				var errors = arguments[1].errors;
+				var keys = _.keys( errors );
+
+				equal( _.keys( errors[keys[0]] )[0], 'required', 'Form has required error' );
 				done();
 			});
 
