7c69d590ed5b3367e70ceab879e3344c1b232c0e
Taylor Lovett <tlovett88@gmail.com>
2015-03-04 00:38:46 -0500

    Deal with title special chars

--- build/js/form-manager.js
+++ build/js/form-manager.js
@@ -306,6 +306,7 @@
 						.replace( /&#8220;/g, '”' )
 						.replace( /&#8221;/g, '”' )
 						.replace( /&#8216;/g, "‘" )
+						.replace( /&#038;/g, "&" )
 						.replace( /&#039;/g, "'" );
 
 					this.set( key, value );
