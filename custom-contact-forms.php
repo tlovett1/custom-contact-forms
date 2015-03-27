8b8c3b20248cd108d6df0f47ed2b4daca337de76
Taylor Lovett <tlovett88@gmail.com>
2015-03-27 10:20:52 -0400

    Version 6.5.2

--- custom-contact-forms.php
+++ custom-contact-forms.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://www.taylorlovett.com
  * Description: Build beautiful custom forms the WordPress way. View live previews of your forms while you build them.
  * Author: Taylor Lovett
- * Version: 6.5.1
+ * Version: 6.5.2
  * Author URI: http://www.taylorlovett.com
  */
 
