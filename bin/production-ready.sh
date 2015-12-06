8d2c9ae0e91dad8a1536aa952330bf4e1741b13d
Taylor Lovett <tlovett88@gmail.com>
2015-12-06 14:39:09 -0500

    Cleanup unneeded files

--- bin/production-ready.sh
+++ bin/production-ready.sh
@@ -14,8 +14,8 @@ if [ "$input_variable" == "y" ]; then
     rm -rf node_modules > /dev/null 2>&1
     rm -rf scss > /dev/null 2>&1
     rm -rf tests > /dev/null 2>&1
+    rm -rf vendor > /dev/null 2>&1
     rm -rf .sass-cache > /dev/null 2>&1
-    rm -rf vendor/wp-api/wp-api/.git > /dev/null 2>&1
 
     rm .travis.yml > /dev/null 2>&1
     rm Gruntfile.js > /dev/null 2>&1
